9aa4c4547f29abbdf7ad6874548e79eb85877933
Tiffany Brand <brand2learn@gmail.com>
2020-07-14 12:32:57 -0400

    Add hall of fame functionality

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ let ans4El = document.querySelector("#ans-4")
 let rightWrongEl = document.querySelector("#right-wrong");
 let endDivEl = document.querySelector(".end-div");
 let finalScoreEl = document.querySelector("#final-score");
+let scoreForm = document.querySelector("#score-form");
+let initialsInput = document.querySelector("#initials");
+let scoresList = document.querySelector("#scores-list");
+let hallOfFame = document.querySelector(".hall-of-fame");
+let hofLink = document.querySelector("div#hof");
 
 
 const questionBank = [
@@ -99,8 +104,11 @@ function nextQuestion() {
         // call displayQuestion(questionId)
         displayQuestion();
     } else {
-        // display end screen
-        endQuiz();
+        // display end screen after showing right or wrong
+        setTimeout(function () {
+            endQuiz();
+        }, 1000)
+
     }
 }
 
@@ -113,12 +121,93 @@ function endQuiz() {
 
 }
 
+// Hall of Fame 
+
+let scores = [];
+
+// Function used to compare scores in order to sort them in decending order
+// Object sorting code and explanation found at Quick Tip: How to Sort and Array of Objects
+// in JavaScript https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+function compareScores(a, b) {
+    const score1 = a.score;
+    const score2 = b.score;
+
+    let compare = 0;
+    if (score1 < score2) {
+        compare = 1;
+    } else if (score1 > score2) {
+        compare = -1
+    };
+    return compare;
+}
+
+function renderScores() {
+    // hide other screens 
+    questionDivEl.setAttribute("style", "display: none;");
+    endDivEl.setAttribute("style", "display: none;");
+    startDivEl.setAttribute("style", "display: none;");
+
+    // clear current scores on page
+    scoresList.innerHTML = "";
+
+    // sort scores in order from highest to lowest
+    scores.sort(compareScores);
+    // render scores on page in LIs
+    for (let i = 0; i < scores.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = `${scores[i].initials} - ${scores[i].score}`;
+        scoresList.appendChild(li);
+    }
+    // show Hall of Fame Screen
+    hallOfFame.setAttribute("style", "display: block;");
+
+}
+
+function storeScore() {
+    // add initials and score
+    localStorage.setItem("scores", JSON.stringify(scores));
+}
+
+function loadScores() {
+    // check for scores in local Storage
+    const storedScores = JSON.parse(localStorage.getItem("scores"));
+    if (storedScores) {
+        scores = storedScores;
+    }
+
+}
+
+scoreForm.addEventListener("submit", function () {
+    event.preventDefault();
+    let initials = initialsInput.value.trim();
+
+    if (!initials) {
+        return;
+    }
+
+
+    let initialsScore = { initials: initials, score: secondsLeft };
+
+    scores.push(initialsScore);
+
+    storeScore();
+    renderScores();
+})
+
+
+// Load any high scores from local Storage before beginning the quiz
+loadScores();
+
 answerButtons.addEventListener("click", function () {
     let element = event.target;
     if (element.matches("button")) {
         console.log(element.value);
         checkAnswer(element.value);
     }
+})
+
+hofLink.addEventListener("click", function () {
+    renderScores();
 })
 
 startButton.addEventListener("click", startQuiz);
